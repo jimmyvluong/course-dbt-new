fdc97bca9df29679a838b15bab5094cb71604d24
Jimmy Luong <jimmyvluong@gmail.com>
2022-06-27 21:35:39 +0000

    order by desc for conversion

--- greenery/models/marts/product/fct_conversion_by_product.sql
+++ greenery/models/marts/product/fct_conversion_by_product.sql
@@ -9,3 +9,4 @@ SELECT
   , ROUND(SUM(add_to_cart) / count(distinct session_id),2) AS product_conversion_rate
   FROM {{ ref('int_session_events_agg') }} 
   GROUP BY 1,2
+  ORDER BY product_conversion_rate DESC
